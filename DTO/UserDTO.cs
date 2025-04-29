264350bd2f7b37805068ca95ee5efdf123d5cb63
Dark_ezt <contactcreativing@gmail.com>
2025-04-29 11:35:33 -0500

    API
    todos los archivos

---
+++ DTO/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Basic_Web_Api.DTO
+{
+    public class UserDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+}
---
+++ DbContext/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Basic_Web_Api.DbContext
+{
+    using Basic_Web_Api.Entity;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
